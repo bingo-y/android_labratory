53aa07b2a595aa18679582cab269f7fbf53eb6d9
tuyx <tuyaoxing@kuaiqiangche.com>
2016-10-18 19:06:47 +0800

    搜索view

--- app/src/main/java/com/bingo/laboratory/ui/canvas/CanvasActivity.java
+++ app/src/main/java/com/bingo/laboratory/ui/canvas/CanvasActivity.java
@@ -21,18 +21,22 @@ public class CanvasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        PaintView pv = new PaintView(this);
 //        ManualPaintView manualPaintView = new ManualPaintView(this);
-        taijiView = new TaijiView(this);
-        setContentView(taijiView);
-        final Handler handler = new Handler() {
-            private float degrees = 0;
-            @Override
-            public void handleMessage(Message msg) {
-                super.handleMessage(msg);
-                taijiView.setRotate(degrees += 5);
-                this.sendEmptyMessageDelayed(0, 80);
-            }
-        };
 
-        handler.sendEmptyMessageDelayed(0, 20);
+//        taijiView = new TaijiView(this);
+//        setContentView(taijiView);
+//        final Handler handler = new Handler() {
+//            private float degrees = 0;
+//            @Override
+//            public void handleMessage(Message msg) {
+//                super.handleMessage(msg);
+//                taijiView.setRotate(degrees += 5);
+//                this.sendEmptyMessageDelayed(0, 80);
+//            }
+//        };
+//
+//        handler.sendEmptyMessageDelayed(0, 20);
+
+        SearchView searchView = new SearchView(this);
+        setContentView(searchView);
     }
 }
